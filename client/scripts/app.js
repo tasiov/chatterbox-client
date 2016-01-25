9652934b799ce7a5b0eb4111d4e2fc16d5594192
tasiov <tasiovictoria@gmail.com>
2016-01-25 15:47:23 -0800

    implemented functions and begins communicating with api

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,7 +7,14 @@ var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
 
   init: function() {
+    var self = this;
 
+    $('.username').on('click', function(event) {
+      var username = event.currentTarget.innerHTML;
+      self.addFriend(username);
+    });
+
+    $('#send .submit').on('submit', self.handleSubmit);
   },
 
   send: function(message) {
@@ -24,7 +31,17 @@ var app = {
 
     $.ajax({
       type: "GET",
-      url: this.server
+      url: this.server,
+      data: 'order=-createdAt',
+      success: function(data){
+        console.log('data', data.results);
+        var results = data.results;
+
+        for( var i in results ) {
+          var result = results[i];
+          this.addMessage(result);
+        }
+      }.bind(this)
     });
   },
 
@@ -43,6 +60,16 @@ var app = {
   addRoom: function(roomName) {
     var div = "<div>" + roomName + "</div>";
     $('#roomSelect').append(div);
+  },
+
+  addFriend: function(username) {
+    console.log(username);
+  },
+
+  handleSubmit: function() {
+    // grab data from text box
+    // clear text box
+    // use this.send to upload message to server
   }
 
 };
